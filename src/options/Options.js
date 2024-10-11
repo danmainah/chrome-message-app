f4d22ab53fb80dcfaf934b8991b7d327a642dba3
Daniel Maina <danielmainawaweru@gmail.com>
2024-10-11 12:11:25 +0300

    setup the app structure

---
+++ src/options/Options.js
@@ -0,0 +1,10 @@
+const Options = () => {
+
+    return (
+        <div>
+            <h1>Options</h1>
+        </div>
+    )
+}
+
+export default Options
--- src/options/index.js
+++ src/options/index.js
@@ -1,13 +1,10 @@
+import Options from "./Options";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Options />
   </React.StrictMode>
 );
-
